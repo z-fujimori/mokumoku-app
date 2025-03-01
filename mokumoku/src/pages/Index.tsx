5025427d8f7ce9ffa0e57bab954363e3551f10f4
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-03-02 01:16:43 +0900

    tailwind導入

--- mokumoku/src/pages/Index.tsx
+++ mokumoku/src/pages/Index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>
+      <h1>みだし</h1>
+      <div className='p-10 m-10'>a</div>
+      <div className=''>a</div>
+      <div className=''>a</div>
+      <div className='m-10'>a</div>
+    </div>
   )
 }
 
