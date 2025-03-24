66077018915482bd75c7353e9d683be31c126589
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-03-24 17:12:54 +0900

    archive item

---
+++ mokumoku/src/components/archive/ArchiveItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArchiveItem = () => {
+    return (
+        <div className='w-3/4 h-32 border rounded-lg'>
+            <div className='h-full flex items-center'>
+                <p className='ml-5'>タスク名</p>
+            </div>
+        </div>
+    )
+}
+
+export default ArchiveItem
--- mokumoku/src/pages/Archive.tsx
+++ mokumoku/src/pages/Archive.tsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import { invoke } from '@tauri-apps/api/core'
 import { useState } from 'react';
+import ArchiveItem from '../components/archive/ArchiveItem';
 
 const Archive = () => {
     const [demoEnv, setDemoEnv] = useState("button");
@@ -10,9 +11,19 @@ const Archive = () => {
     }
 
     return (
-        <div className='h-auto justify-center items-center'>
+        <div className='h-auto w-full justify-center items-center'>
             <h1 className='text-xl'>開発中...</h1>
             <button onClick={handleButton}>{demoEnv}</button>
+            <div className='w-full h-72 overflow-y-auto'>
+                <div className='flex flex-col items-center justify-center'>
+                    <ArchiveItem />
+                    <ArchiveItem />
+                    <ArchiveItem />
+                    <ArchiveItem />
+                    <ArchiveItem />
+                    <ArchiveItem />
+                </div>
+            </div>
         </div>
     )
 }
